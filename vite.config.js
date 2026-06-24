1e99f8e497cb06efe73da23a7d845d9b7b15f60e
UNIGO777 <naman13399@gmail.com>
2026-06-25 00:58:51 +0530

    asdf

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://api.phxcrossborder.com',
         changeOrigin: true,
       },
     },
